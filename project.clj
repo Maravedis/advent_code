785a2bae304ae71a556fde69f0f6c486f731ed3a
Clément Malaingre <clement.malaingre@oscaro.com>
2022-12-17 01:27:47 +0100

    day16 - Please don't hit me anymore

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject advent_of_code "1.0.0"
   :description "The 2022 advent of code event"
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/core.match "1.0.0"]]
+                 [org.clojure/core.match "1.0.0"]
+                 [org.clojure/math.combinatorics "0.1.6"]
+                 [com.rpl/specter "1.1.4"]]
   :resource-paths ["resources"]
   :profiles {:dev {:source-paths   ["test" "dev"]
                    :resource-paths ["dev-resources"]}})
---
+++ src/advent_of_code/day16.clj
@@ -0,0 +1,78 @@
+(ns advent-of-code.day16
+  (:require [advent-of-code.utils :as u]
+            [clojure.string :as str]))
+
+(defn read-valve [line]
+  (let [s (str/split line #"([;,]? )|=")
+        coor-fn (fn [i] (first (drop i s)))]
+    [(coor-fn 1) {:rate  (read-string (coor-fn 5))
+                  :conns (->> (drop 10 s)
+                              (map (fn [k] [k 1]))
+                              (into {}))}]))
+
+(defn compress-graph [graph] 
+  (loop [graph graph
+         i     0]
+    (if-let [[k {:keys [conns]}] (first (drop-while (fn [[k {:keys [rate]}]] (or (= k "AA") (not= rate 0))) graph))] 
+      (recur (dissoc (->> (map (fn [[k' v]]
+                                 (if (contains? (:conns v) k)
+                                   [k' (-> (update v :conns
+                                                   #(reduce (fn [acc1 [dest cost]]
+                                                              (if (not= dest k')
+                                                                (assoc acc1 dest (+ cost (get-in v [:conns k])))
+                                                                acc1))
+                                                            % conns))
+                                           (update :conns #(dissoc % k)))]
+                                   [k' v]))
+                               graph)
+                          (into {}))
+                     k)
+             (inc i))
+      graph)))
+
+(defn distances [graph]
+  (let [nodes  (sort (keys graph))
+        dist-i (->> (map (fn [i]
+                           [i (->> (map (fn [j] [j (get-in graph [i :conns j] 1000)]) nodes)
+                                   (into {}))])
+                         nodes)
+                    (map (fn [[k v]] [k (assoc v k 0)]))
+                    (into {}))]
+    (loop [[hi & ti :as i] nodes
+           [hj & tj :as j] nodes
+           [hk & tk :as k] nodes
+           dist            dist-i]
+      (cond (nil? hi) dist
+            (nil? hj) (recur ti nodes nodes dist)
+            (nil? hk) (recur i tj nodes dist)
+            :else (recur i j tk
+                         (update-in dist
+                                    [hi hj]
+                                    #(min % (+ (get-in dist [hi hk]) (get-in dist [hk hj])))))))))
+
+(defn fix [f] (fn g [& args] (apply f g args))) ; fix inline memoization, thanks stack overflow
+
+
+(defn run [path part2?]
+  (let [input (->> (u/read-file-list path read-valve)
+                   (into {})
+                   (compress-graph))
+        time  (if part2? 26 30)
+        dist  (distances input)
+        dfs   (fn [dfs valve minutes opened first?]
+                (->> input
+                     (filter (fn [[node {:keys [rate]}]] (and (not (contains? opened node))
+                                                              (not= 0 rate)
+                                                              (< 0 (get-in dist [valve node]) minutes))))
+                     (map (fn [[node {:keys [rate]}]]
+                            (let [d         (inc (get-in dist [valve node]))
+                                  time-left (- minutes d)]
+                              (+ (* time-left rate) (dfs node time-left (conj opened valve) first?)))))
+                     (reduce max (if first? (dfs "AA" time (conj opened valve) false) 0)))) 
+        dfs-m (fix (memoize dfs))]
+    (dfs-m "AA" time #{"AA"} part2?)))
+  
+(comment
+  (time (run "day16" false))
+  (time (run "day16" true))
+  )
