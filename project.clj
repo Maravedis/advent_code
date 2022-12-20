bdc87e91021b7a9d59eee26b5bb51bacd1d91ba6
Clément Malaingre <clement.malaingre@oscaro.com>
2022-12-20 12:16:02 +0100

    day20

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/core.match "1.0.0"]
                  [org.clojure/math.combinatorics "0.1.6"]
-                 [com.rpl/specter "1.1.4"]]
+                 [com.rpl/specter "1.1.4"]
+                 [org.clojure/core.rrb-vector "0.1.2"]]
   :resource-paths ["resources"]
   :profiles {:dev {:source-paths   ["test" "dev"]
                    :resource-paths ["dev-resources"]}})
