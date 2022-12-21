bd655d1493eabc1aba1c2d4cfd0b7a5ea500511e
Clément Malaingre <clement.malaingre@oscaro.com>
2022-12-21 19:18:04 +0100

    21, another approach based on smarter people things

--- src/advent_of_code/day21.clj
+++ src/advent_of_code/day21.clj
@@ -1,6 +1,7 @@
 (ns advent-of-code.day21
   (:require [advent-of-code.utils :as u]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.math :refer [signum]]))
 
 (defn read-monkey [line]
   (let [arr (str/split line #"[:]? ")]
@@ -21,6 +22,27 @@
         dfs-m (u/fix (memoize dfs))]
     (dfs-m :root)))
 
+; I wrote this after looking at solutions. Really didn't think of bruteforce, tried to mvoe the tree around to solve it equation like. DIdn't go over well.
+(defn part2 [path]
+  (let [tree  (-> (into {} (u/read-file-list path read-monkey))
+                  (assoc-in [:root :op] "-"))
+        dfs   (fn [dfs tree curr]
+                (let [node (tree curr)]
+                  (if (int? node)
+                    node
+                    ((resolve (symbol (:op node))) (dfs tree (:left node)) (dfs tree (:right node))))))
+        dfs-m (u/fix (memoize dfs))
+        init-sign (signum (dfs-m tree :root))]
+    (loop [lo 0
+           hi 99999999999999]
+      (let [mid      (quot (+ lo hi) 2)
+            monkeys  (assoc tree :humn mid)
+            root-val (dfs-m monkeys :root)]
+        (cond 
+          (zero? root-val) mid
+          (= (signum root-val) init-sign) (recur (inc mid) hi)
+          :else (recur lo mid))))))
+
 (defn nest [graph curr]
   (cond
     (= curr :humn) (symbol "x")
@@ -38,6 +60,7 @@
 
 (comment
   (time (part1 "day21")) 
+  (time (part2 "day21"))
   (->  (into {} (u/read-file-list "day21" read-monkey))
        (nest :root)
        (assoc :op "=")
