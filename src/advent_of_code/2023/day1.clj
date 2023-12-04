0ff41ecc3fa41193e16112f83f7c99a8bddf5319
Clément Malaingre <clement.malaingre@oscaro.com>
2023-12-04 10:50:19 +0100

    faster day1

--- src/advent_of_code/2023/day1.clj
+++ src/advent_of_code/2023/day1.clj
@@ -1,29 +1,18 @@
 (ns advent-of-code.2023.day1
-  (:require [advent-of-code.utils :as u]
-            [clojure.string :as str]))
+  (:require [advent-of-code.utils :as u]))
 
-(def reg (re-pattern (str "^" (str/join "|^" (keys u/digit-map)))))
-
-(defn find-first-occurence [input parse-fn] 
-  (loop [[h & t] input]
-    (if-let [res (parse-fn h)]
-      res
-      (recur t))))
-
-(defn ->digit [[h]] (u/char->digit h))
-(defn ->number [l] (or (->digit l) (u/digit-map (re-find reg l))))
-
-(defn first-and-last [f line]
-  (let [l     (count line)
-        first (find-first-occurence (for [i (range 0 l)] (subs line i l)) f) 
-        last  (find-first-occurence (for [i (range (dec l) -1 -1)] (subs line i l)) f)] 
-    (+ (* 10 first) last)))
-
-(defn gen-solve [path f]
-  (->> (u/read-file-list path (partial first-and-last f)) 
+(defn part1 [path]
+  (->> (u/read-file-list path #(re-seq #"\d" %))
+       (map #(+ (* 10 (parse-long (first %))) (parse-long (last %))))
        (apply +)))
 
-(comment
-  (time (gen-solve "2023/1.in" ->digit))
-  (time (gen-solve "2023/1.in" ->number))
+(defn part2 [path]
+  (->> (u/read-file-list path #(re-seq #"(?=(one|two|three|four|five|six|seven|eight|nine|\d))" %))
+       (map #(+ (* 10 (u/digit-map (second (first %)))) (u/digit-map (second (last %)))))
+       (apply +)))
+
+(comment 
+  (part1 "2023/1.in") ; ~857µs
+  (part2 "2023/1.in") ; ~3.2ms 
   )
+  
