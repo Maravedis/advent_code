994a37b86d06df0ac9b0961e35bc9215e4f02556
Clément Malaingre <clement.malaingre@oscaro.com>
2023-12-06 14:06:35 +0100

    faster day 5

--- src/advent_of_code/2023/day5.clj
+++ src/advent_of_code/2023/day5.clj
@@ -10,30 +10,24 @@
   (let [[seeds & maps] (str/split (slurp (io/resource path)) #"\n\n")]
     [(u/nums seeds) (map #(->> (str/split-lines %) (drop 1) (map u/nums) (sort-by second) pad) maps)]))
 
-(defn next-step [[ax l] [[d sx r :as m] & ms]]
-  (if (nil? m)
-    [[ax l]]
-    (let [ay (+ ax l)
-          sy (+ sx r)]
-      (cond
-        (and (< ax sx) (<= sx ay sy)) [[d (- ay sx)]]
-        (<= ax sx sy ay)              (conj (next-step [sy (- ay sy)] ms) [d r])
-        (and (< sx ax) (<= ax sy ay)) (conj (next-step [sy (- ay sy)] ms) [(+ d (- ax sx)) (- sy ax)])
-        (<= sx ax ay sy)              [[(+ d (- ax sx)) l]]
-        :else (next-step [ax l] ms)))))
-
 (defn gen-solve [path part2?]
   (let [[seeds maps] (read-almanach path)]
     (loop [[sh & st :as steps] maps
-           [h & t :as ss]      (if part2? (partition 2 seeds) (map (fn [seed] [seed 1]) seeds))
-           result              []]
-      (if (nil? sh)
-        (apply min (map first ss))
-        (if (nil? h)
-          (recur st result [])
-          (recur steps t (concat (next-step h sh) result)))))))
+           [h & t :as ss]      (apply sorted-set (if part2? (map vec (partition 2 seeds)) (map (fn [seed] [seed 1]) seeds)))
+           next-step           (sorted-set)]
+      (if-let [[[d sx r] & ms] sh]
+        (if-let [[ax l] h]
+          (let [ay (+ ax l)
+                sy (+ sx r)]
+            (cond
+              (and (> ax sy) ms) (recur (cons ms st) ss next-step)
+              (> ax sy)  (recur st (apply conj next-step ss) (sorted-set))
+              (<= ay sy) (recur steps t (conj next-step [(+ d (- ax sx)) l]))
+              :else (recur (cons ms st) (conj t [sy (- ay sy)]) (conj next-step [(+ d (- ax sx)) (- sy ax)]))))
+          (recur st next-step (sorted-set)))
+        (apply min (map first ss))))))
 
-(comment 
-  (gen-solve "2023/5.in" false) ; ~1.7ms
-  (gen-solve "2023/5.in" true)  ; ~3ms
+(comment
+  (gen-solve "2023/5.in" false) ; ~740µs
+  (gen-solve "2023/5.in" true)  ; ~1.1ms
   )
