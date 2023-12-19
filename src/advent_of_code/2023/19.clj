de28cd8b634ab613dc1becb6fe5a41d202c23d0d
Clément Malaingre <clement.malaingre@oscaro.com>
2023-12-19 13:57:37 +0100

    2023 19

--- src/advent_of_code/2023/19.clj
+++ src/advent_of_code/2023/19.clj
@@ -1,20 +1,31 @@
 (ns advent-of-code.2023.19
   (:require [advent-of-code.utils :as u]))
 
+(def var-idx {"x" 0
+              "m" 1
+              "a" 2
+              "s" 3})
+
 (defn parse-rule [rule]
   (let [[_ name conds fail] (re-find #"(\w+)\{(.*),(\w+)" rule)
-        rules                 (->> (re-seq #"([xmas])([<>])(\d+):(\w+)" conds)
-                                   (map (fn [[_ car comp num dest]] 
-                                          (fn [piece] 
-                                            (when ((case comp ">" > "<" <) (piece car) (parse-long num)) dest)))))]
-    [name {:conds rules
-           :fail  fail}]))
+        conds (re-seq #"([xmas])([<>])(\d+):(\w+)" conds)
+        rules (map (fn [[_ car comp num dest]]
+                     (fn [piece]
+                       (when ((case comp ">" > "<" <) (piece car) (parse-long num)) dest))) conds)]
+    [name {:conds    rules
+           :explicit (map (fn [[_ car comp num dest]]
+                            [(var-idx car) comp (parse-long num) dest]) conds)
+           :fail     fail}]))
+
 
 (defn parse-piece [piece]
   (let [values (re-seq #"([xmas])=(\d+)" piece)]
     (->> values
          (map (fn [[_ k v]] [k (parse-long v)]))
          (into {}))))
+
+(defn calc-combinations [[x-min x-max m-min m-max a-min a-max s-min s-max]]
+  (* (- x-max x-min) (- m-max m-min) (- a-max a-min) (- s-max s-min)))
 
 (defn part1 [path]
   (let [[rules pieces] (u/read-file-segmented-list path identity)
@@ -33,35 +44,32 @@
             (recur xs v accepted)))
         (reduce (fn [res p] (apply + res (vals p))) 0 (persistent! accepted))))))
 
-
-(defn part2 [path]
-  (let [[rules _] (u/read-file-segmented-list path identity)
-        rules          (->> (map parse-rule rules)
-                            (into {}))
-        pieces (for [x (range 1 4001)
-                     m (range 1 4001)
-                     a (range 1 4001)
-                     s (range 1 4001)] {"x" x "m" m "a" a "s" s})]
-    (loop [[h & t :as xs] pieces
-           curr           "in"
-           accepted       (transient [])]
-      (if h
-        (let [{:keys [conds fail]} (rules curr)
-              v                    (or (first (keep #(% h) conds)) fail)]
-          (case v
-            "A" (recur t "in" (conj! accepted h))
-            "R" (recur t "in" accepted)
-            (recur xs v accepted)))
-        (count accepted)))))
+; ranges [x-min x-max m-min m-max a-min a-max s-min s-max]
+(defn part2 
+  ([path]
+   (let [[rules _] (u/read-file-segmented-list path identity)
+         rules     (->> (map parse-rule rules)
+                        (into {}))]
+     (part2 rules "in" [1 4001 1 4001 1 4001 1 4001])))
+  ([rules curr ranges]
+   (cond (= "A" curr) (calc-combinations ranges)
+         (= "R" curr) 0
+         :else
+         (let [wf           (-> (get rules curr) :explicit)
+               fail         (-> (get rules curr) :fail)
+               [failed res] (reduce (fn [[r res] [car comp num dest]]
+                                      (case comp
+                                        ">" [(assoc r (inc (* 2 car)) (inc num))
+                                             (+ res (part2 rules dest (assoc r (* car 2) (inc num))))]
+                                        "<" [(assoc r (* 2 car) num)
+                                             (+ res (part2 rules dest (assoc r (inc (* car 2)) num)))]))
+                                    [ranges 0] wf)]
+           (+ res (part2 rules fail failed))))))
 
 (comment
   (def path (u/get-input 2023 19))
   (def tpath "2023/19_test.in")
 
-  (parse-rule "px{a<2006:qkq,m>2090:A,rfg}")
-  (parse-piece "{x=787,m=2655,a=1222,s=2876}")
-
   (part1 path)
-  (* 4000 4000 4000 4000)
-  (part2 tpath)
+  (part2 path)
   )
