0879b6d40addcb2f11ce9847e410f04f3a22da56
Clément Malaingre <clement.malaingre@oscaro.com>
2024-12-07 15:56:58 +0100

    Steal every optimization for 2024 07!

--- src/advent_of_code/2024/07.clj
+++ src/advent_of_code/2024/07.clj
@@ -1,28 +1,37 @@
 (ns advent-of-code.2024.07
   (:require [advent-of-code.utils :as u]
-            [clojure.core.reducers :as r]))
+            [clojure.core.reducers :as r]
+            [clojure.string :refer [ends-with?]]
+            [criterium.core :refer [quick-bench]]))
 
 (def path (u/get-input 2024 7))
 (def tpath "2024/7_test.in")
+(def input (vec (u/read-file-list path u/nums)))
 
-(defn || [a b]
-  (parse-long (str a b)))
+(defn *-1 [x y] (when (zero? (rem x y)) (quot x y)))
+(defn ||-1 [x y]
+  (let [xs (str x)
+        ys (str y)]
+    (when (ends-with? xs ys)
+      (parse-long (subs xs 0 (- (count xs) (count ys)))))))
 
-(defn calc [part2? [total f1 & fs]]
-  (let [ops (apply juxt (if part2? [+ * ||] [+ *]))]
+(defn calc [part2? [total & factors]]
+  (let [ops (apply juxt (if part2? [- *-1 ||-1] [- *-1]))
+        fs  (vec (reverse factors))]
     (->> (reduce (fn [states v]
                    (->> (r/mapcat #(ops % v) states)
-                        (r/remove #(> % total))
-                        r/foldcat)) [f1] fs)
-         (some #(= total %)))))
+                        (r/remove #((fnil < -1) % 0))
+                        r/foldcat)) [total] fs)
+         (some #(= 0 %)))))
 
-(defn solve [path part2?]
-  (let [input (vec (u/read-file-list path u/nums))]
-    (->> (r/filter (partial calc part2?) input)
-         (r/map first)
-         (r/fold 100 + +))))
+(defn solve [input part2?]
+  (->> (r/filter (partial calc part2?) input)
+       (r/map first)
+       (r/fold 100 + +)))
 
 (comment
-
-  (time (solve path false))
-  (time (solve path true)))
+  (quick-bench (solve input false)) ; ~2.4ms
+  ; 1582598718861
+  (quick-bench (solve input true))  ; ~4.9ms
+  ; 165278151522644
+  )
