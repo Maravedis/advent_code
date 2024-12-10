7201b419b8336b5bc5148eb5a37a15fcba3649b5
Clément Malaingre <clement.malaingre@oscaro.com>
2024-12-10 08:01:11 +0100

    Cleaner 2024 10

--- src/advent_of_code/2024/10.clj
+++ src/advent_of_code/2024/10.clj
@@ -11,31 +11,19 @@
                  (filter (fn [[_ v]] (= 0 v)))
                  (map first)))
 
-(defn reachable-ends [grid start cur]
-  (if (= 9 (grid start))
-    [start]
-    (->> p/cardinals
-         (map #(p/move start %))
-         (filter #(when-let [num (grid %)] (= num (+ cur 1))))
-         (mapcat #(reachable-ends grid % (+ cur 1)))
-         set)))
-
-(defn count-distinct-trail [grid start cur]
-  (if (= 9 (grid start))
-    1
-    (->> p/cardinals
-         (map #(p/move start %))
-         (filter #(when-let [num (grid %)] (= num (+ cur 1))))
-         (map #(count-distinct-trail grid % (+ cur 1)))
-         (reduce +))))
+(defn reachable-ends [grid start]
+  (->> (iterate (fn [[level curs]] (->> (mapcat #(map (partial p/move %) p/cardinals) curs)
+                                        (filter #(when-let [num (grid %)] (= num (+ 1 level))))
+                                        (vector (inc level)))) [0 [start]])
+       (drop 9) first second))
 
 (defn part1 []
-  (->> (map #(reachable-ends grid % 0) starts)
-       (reduce #(+ %1 (count %2)) 0)))
+  (->> (map #(reachable-ends grid %) starts)
+       (reduce #(+ %1 (count (set %2))) 0)))
 
 (defn part2 []
-  (->> (map #(count-distinct-trail grid % 0) starts)
-       (reduce +)))
+  (->> (map #(reachable-ends grid %) starts)
+       (reduce #(+ %1 (count %2)) 0)))
 
 (comment
   (part1)
