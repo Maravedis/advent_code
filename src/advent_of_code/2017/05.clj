6ee68c70b9b5639527a1d5966fa2dc012abaa812
Clément Malaingre <malaingreclement@gmail.com>
2025-01-11 13:44:26 +0100

    2017 5

---
+++ src/advent_of_code/2017/05.clj
@@ -0,0 +1,23 @@
+(ns advent-of-code.2017.05
+  (:require [advent-of-code.utils :as u]
+            [advent-of-code.points :as p]))
+
+(def path (u/get-input 2017 5))
+(def tpath (u/test-path 2017 5))
+
+(defn run [path part2?]
+  (let [input (u/read-file-list path parse-long)
+        n     (count input)]
+    (loop [program (vec input)
+           ip      0
+           steps   0]
+      (if (< -1 ip n)
+        (recur (update program ip (if (and part2? (>= (program ip) 3)) dec inc)) (+ ip (program ip)) (inc steps))
+        steps))))
+
+(comment
+
+  (run path false)
+  (run path true)
+  ;
+  )
